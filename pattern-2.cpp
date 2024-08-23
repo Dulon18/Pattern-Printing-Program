a56a6dc54aa85ded50f8b17c0cb532df4e615d61
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2024-08-23 12:32:58 +0600

    Create pattern-2.cpp

---
+++ pattern-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+void pattern(int n) 
+{
+    for(int row = 1; row <= n; row++)
+    {
+        for(int col = 1; col <= row; col++)
+        {
+            std::cout << "*"; 
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    
+    pattern(4); 
+    return 0;
+  
+}
+
+
+Output : Triangle Star Pattern
+---------
+*
+**
+***
+****
