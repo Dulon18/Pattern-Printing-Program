c8e228f2f282726dac9f6a538444f7c93e005cbc
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2024-08-27 23:50:02 +0600

    Create pattern11.cpp

---
+++ pattern11.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+void pattern(int n) 
+{
+    for(int row = 1; row <n; row++)
+    {
+        for(int space=n;space>row+1;space--)
+        {
+                std::cout<<" "; 
+        }
+        
+        for(int col = 0; col<=row*2-1; col++)
+        {
+            if(row==n-1)
+            {
+                std::cout<<"*"; 
+            }
+            else
+            {
+                if(col==0||col==row*2-2)   
+                     {
+                       std::cout<<"*";
+                     }
+                  else    
+                     {
+                          std::cout<<" "; 
+                     }
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    
+    pattern(8); 
+    return 0;
+}
