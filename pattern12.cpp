abb063c6b5a352af3e40bec8c0bf2b05ba66b73c
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2024-11-01 22:20:29 +0600

    Create pattern12.cpp

---
+++ pattern12.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int start, rows;
+    
+    cout << "Enter the starting number: ";
+    cin >> start;
+    
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    for (int i = 0; i < rows; i++) {
+        int num = start + i;
+        for (int j = 0; j <= i; j++) {
+            cout << num;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+Output:
+
+4
+55
+666
+7777
+88888
+999999
